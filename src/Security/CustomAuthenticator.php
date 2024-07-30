fc6f8282b4b2767702db07665953ec71efd87ccf
khauvannam <nkhauvan@gmail.com>
2024-07-30 14:41:52 +0700

    add logi.n

--- src/Security/CustomAuthenticator.php
+++ src/Security/CustomAuthenticator.php
@@ -2,33 +2,62 @@
 
 namespace App\Security;
 
+use App\Repository\Identities\IdentityRepository;
+use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
 use Symfony\Component\Security\Core\Exception\AuthenticationException;
+use Symfony\Component\Security\Core\Exception\UserNotFoundException;
+use Symfony\Component\Security\Core\User\UserInterface;
 use Symfony\Component\Security\Http\Authenticator\AbstractAuthenticator;
+use Symfony\Component\Security\Http\Authenticator\Passport\Badge\RememberMeBadge;
+use Symfony\Component\Security\Http\Authenticator\Passport\Badge\UserBadge;
+use Symfony\Component\Security\Http\Authenticator\Passport\Credentials\PasswordCredentials;
 use Symfony\Component\Security\Http\Authenticator\Passport\Passport;
 
 class CustomAuthenticator extends AbstractAuthenticator
 {
+    private IdentityRepository $identityRepository;
+
+    /**
+     * @param IdentityRepository $identityRepository
+     */
+    public function __construct(IdentityRepository $identityRepository)
+    {
+        $this->identityRepository = $identityRepository;
+    }
+
 
     public function supports(Request $request): ?bool
     {
-        // TODO: Implement supports() method.
+        return ($request->getPathInfo() === '/login' && $request->isMethod('POST'));
     }
 
     public function authenticate(Request $request): Passport
     {
-        // TODO: Implement authenticate() method.
+        $email = $request->request->get('email');
+        $password = $request->request->get('password');
+        return new Passport(
+            new UserBadge($email, function (string $userIdentifier): ?UserInterface {
+                $user = $this->identityRepository->findOneBy(['email' => $userIdentifier]);
+                if (!$user) throw new UserNotFoundException($userIdentifier);
+                return $user;
+            }),
+            new PasswordCredentials($password), [
+                new RememberMeBadge()
+            ]
+        );
     }
 
     public function onAuthenticationSuccess(Request $request, TokenInterface $token, string $firewallName): ?Response
     {
-        // TODO: Implement onAuthenticationSuccess() method.
+        return new RedirectResponse('/');
+
     }
 
     public function onAuthenticationFailure(Request $request, AuthenticationException $exception): ?Response
     {
-        // TODO: Implement onAuthenticationFailure() method.
+        return new RedirectResponse('/login');
     }
 }
