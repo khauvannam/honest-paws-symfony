177579abc59a34039fc41408ec56423a641c66b2
khauvannam <nkhauvan@gmail.com>
2024-08-11 10:53:11 +0700

    done delete cart

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,16 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./assets/**/*.js",
-    "./templates/**/*.html.twig",
-  ],
-  theme: {
-    extend: {
-      colors: {
-        main: "#00205c",
-        hover: "#FADC6F"
-      }
+    content: [
+        "./assets/**/*.js",
+        "./templates/**/*.html.twig",
+    ],
+    theme: {
+        extend: {
+            colors: {
+                main: "#00205c",
+                hover: "#FADC6F"
+            }
+        },
     },
-  },
-  plugins: [],
+    plugins: [
+        require('@tailwindcss/forms'),
+    ],
 }
