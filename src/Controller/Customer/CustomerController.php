52e5de12a670f847d0599b1fdc2857c4a535a007
gN4HT <tranthang75142@gmail.com>
2024-08-14 21:37:33 +0700

    order user

--- src/Controller/Customer/CustomerController.php
+++ src/Controller/Customer/CustomerController.php
@@ -4,21 +4,34 @@ declare(strict_types=1);
 
 namespace App\Controller\Customer;
 
+use App\Features\Orders\Query\GetOrdersByCustomerId;
+use App\Services\GetEnvelopeResultService;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Messenger\MessageBusInterface;
 use Symfony\Component\Routing\Attribute\Route;
 
 class CustomerController extends AbstractController
 {
+    private MessageBusInterface $bus;
+
+    public function __construct(MessageBusInterface $bus)
+    {
+        $this->bus = $bus;
+    }
+
     #[Route('/customer')]
     public function index(): Response
     {
         return $this->render('customer/index.html.twig');
     }
 
-    #[Route('/customer/order', name: 'customer_order', methods: ['GET', 'POST'])]
-    public function order(): Response
+    #[Route('/customer/order/{id}', name: 'customer_order', methods: ['GET', 'POST'])]
+    public function order(string $id): Response
     {
-
+        $command = new GetOrdersByCustomerId($id);
+        $handler = $this->bus->dispatch($command);
+        $result = GetEnvelopeResultService::invoke($handler);
+        return $this->render('customer/order.html.twig', ['orders' => $result]);
     }
 }
